de41e7788465ddfe80a1508e20c831adc72a5147
sazzad-albi <92084082+sazzad-albi@users.noreply.github.com>
2021-11-11 00:07:14 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
     //Owl Carousel Blog
     $('.owl-carousel').owlCarousel({
         loop:true,
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
